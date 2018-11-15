724183645839b6fc08878dd4a7eea70464c4da98
Thomas Deniffel <tdeniffel@googlemail.com>
2018-11-15 16:43:49 +0100

    working

---
+++ src/main/java/io/deniffel/raytracer/Tuple.java
@@ -0,0 +1,16 @@
+package io.deniffel.raytracer;
+
+public class Tuple {
+
+    public final double x;
+    public final double y;
+    public final double z;
+    public final double w;
+
+    public Tuple(double x, double y, double z, double w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+}
--- src/test/java/io/deniffel/raytracer/TupleTests.java
+++ src/test/java/io/deniffel/raytracer/TupleTests.java
@@ -1,10 +1,19 @@
 package io.deniffel.raytracer;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class TupleTests {
     @Test
-    public void toupleWithW1IsAPoint() {
-
+    public void tupleConstructsCorrectly() {
+        Tuple t = new Tuple(4.3, -4.2, 3.1, 1.0);
+        assertEquals(4.3, t.x, 0.0001);
+        assertEquals(-4.2, t.y, 0.0001);
+        assertEquals(3.1, t.z, 0.0001);
+        assertEquals(1.0, t.w, 0.0001);
     }
+
+    // tupleWithW1IsAPoint
 }
